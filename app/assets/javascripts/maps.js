8b15923de130615104125ebee7d4733113d3f7e9
Tim <tim.blonski@gmail.com>
2013-08-05 18:27:21 -0400

    added marker setting functionality with foursquare api.

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -2,6 +2,8 @@
 var EMAPS = {
   latlng: {}
 };
+var squareInfo = '';
+var markerArray = [];
 
 $('#map').click(function(event) {
     cord = EMAPS.latlng.val();
@@ -36,39 +38,52 @@ $(function () {
   }).addTo(map);
 
   // add tile layer, set the location and zoom
-  var bx = new L.LatLng(40.715281, -73.990209); // geographical point (longitude and latitude)
-  map.setView(bx, 12).addLayer(layer);
+  // var bx = new L.LatLng(40.715281, -73.990209); // geographical point (longitude and latitude)
+  // map.setView(bx, 12).addLayer(layer);
 
-  // create a marker in the given location and add it to the map
-  var markerLocation = bx;
-  var marker = new L.Marker(markerLocation);
-  map.addLayer(marker);
+
+  //
+  function setLocation(){
+    for (var i = 0; i < squareInfo.response.venues.length; i ++){
+      lat = squareInfo.response.venues[i].location.lat;
+      lng = squareInfo.response.venues[i].location.lng;
+
+      trend = new L.LatLng(lat, lng);
+      marker = new L.Marker(trend);
+      map.addLayer(marker);
+      markerArray.push(marker);
+    }
+  }
+
+  function removeMarker(){
+    for (var i = 0; i < markerArray.length; i ++){
+      map.removeLayer(markerArray[i]);
+    }
+  }
 
 
   var popup = L.popup();
-
   function onMapClick(e) {
+    removeMarker();
     console.log(e.latlng);
       EMAPS.latlng = e.latlng;
       console.log(EMAPS.latlng);
       var lat = EMAPS.latlng.lat;
       var lng = EMAPS.latlng.lng;
       var cord = lat + ',' + lng;
-      var url = 'https://api.foursquare.com/v2/venues/trending?ll=' + cord + '&client_id=FLORXQIYM4IR2BQJQS52RRKJIDTIYE3PVGUXPAEOCRLPLTMF&client_secret=0E30B1EZG3RQK0UMKPIU05LNMSZOOAKVBR4QFOJFO1KAGEEG&v=2013 0316';
+      var url = 'https://api.foursquare.com/v2/venues/trending?ll=' + cord + '&client_id=FLORXQIYM4IR2BQJQS52RRKJIDTIYE3PVGUXPAEOCRLPLTMF&client_secret=0E30B1EZG3RQK0UMKPIU05LNMSZOOAKVBR4QFOJFO1KAGEEG&v=20130316';
         $.ajax({
         type: 'get',
         url: url,
         dataType: 'json'
       }).done(function(data){
         console.log(data);
-      popup
-          .setLatLng(e.latlng)
-          .setContent("You clicked the map at " + e.latlng.toString())
-          .openOn(map);
+        squareInfo = data;
+        setLocation();
     });
   }
   map.on('click', onMapClick);
 
 });
->>>>>>> upstream/master
+
 
