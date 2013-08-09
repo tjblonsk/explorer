1a5a802f2c3dc32be0b784fca086af06818b0b8c
Tim <tim.blonski@gmail.com>
2013-08-09 18:28:25 -0400

    everything is working

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -13,15 +13,15 @@ class MapsController < ApplicationController
       phone: params[:phone],
       website: params[:website]
       )
-     @city_name = params[:city]
-     @state = params[:state]
-     @city_to_search = @city_name + ', ' + @state
-     @city = City.where(name: @city_to_search)
+     city_name = params[:city]
+     state = params[:state]
+     city_to_search = city_name + ', ' + state
+     @city = City.where(name: city_to_search)
      @city.first.spots << @spotFave
      @city.first.favorite = true
-
-    current_user.cities << @city
-    current_user.spots << @spotFave
+     current_user.cities << @city
+     current_user.spots << @spotFave
+     current_user
 
     respond_to do |format|
       format.json {render json: @spotFave}
